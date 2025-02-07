179994139b2e27310563b57044af23f2264ad875
Thomas Kazungu <152265961+kazungujunior@users.noreply.github.com>
2025-02-07 05:11:10 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("sidenav").style.width = "270px"
+    
+}
+function closeNav () {
+    document.getElementById("sidenav").style.width = "0"
+    
+}
